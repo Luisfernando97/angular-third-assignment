b682d0d364ef3b4841a32bb198e1f7133d29dbda
luisfernando <luisfernandoc.martins@gmail.com>
2019-03-13 10:49:25 -0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  styles:[`
+  .morethanfive{
+    color: white;
+    background-color: blue;
+  }
+  `]
 })
 export class AppComponent {
-  title = 'terceira-tarefa';
+
+  showDetails = false;
+  amountOfClicks = [];
+
+  toggleDetails(){
+    this.showDetails = !this.showDetails;
+    this.amountOfClicks.push((this.amountOfClicks.length +1));
+  }
 }
